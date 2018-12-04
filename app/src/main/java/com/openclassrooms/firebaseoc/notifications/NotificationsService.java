c1d9d7d216b60da49b6b6bf9ccbbe410af11c02e
Unknown <laurent.biancone@gmail.Com>
2018-12-04 11:05:29 +0100

    First
    First

---
+++ app/src/main/java/com/openclassrooms/firebaseoc/notifications/NotificationsService.java
@@ -0,0 +1,68 @@
+package com.openclassrooms.firebaseoc.notifications;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.openclassrooms.firebaseoc.MainActivity;
+import com.openclassrooms.firebaseoc.R;
+import com.openclassrooms.firebaseoc.auth.ProfileActivity;
+
+public class NotificationsService extends FirebaseMessagingService {
+
+    private final int NOTIFICATION_ID = 007;
+    private final String NOTIFICATION_TAG = "FIREBASEOC";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage.getNotification() != null) {
+            String message = remoteMessage.getNotification().getBody();
+            // Show notification after received message
+            this.sendVisualNotification(message);
+        }
+    }
+
+    // ---
+
+    private void sendVisualNotification(String messageBody) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(getString(R.string.notification_title));
+        inboxStyle.addLine(messageBody);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_image_notification)
+                        .setContentTitle(getString(R.string.app_name))
+                        .setContentText(getString(R.string.notification_title))
+                        .setAutoCancel(true)
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        .setContentIntent(pendingIntent)
+                        .setStyle(inboxStyle);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Message provenant de Firebase";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
+    }
+}
